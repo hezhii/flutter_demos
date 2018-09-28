b6399b264798d5cff80f6af0621683dda6fd4774
biu <hezhou920@gmail.com>
2018-09-28 16:14:19 +0800

    Navigation basic

---
+++ lib/demo1_navigation.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('First Screen'),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text('Launch second screen'),
+          onPressed: () {
+            Navigator.of(context).push(
+              new MaterialPageRoute(
+                builder: (context) => new SecondScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Second Screen'),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text('Go back!'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
